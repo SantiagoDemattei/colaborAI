9814553fcfdb072a289f127bb9b844df2c685d38
SantiagoDemattei <sdemattei@frba.utn.edu.ar>
2025-06-23 16:06:31 -0300

    init

---
+++ src/main/java/com/colaborai/colaborai/ColaboraiApplication.java
@@ -0,0 +1,13 @@
+package com.colaborai.colaborai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColaboraiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColaboraiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colaborai/colaborai/ColaboraiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colaborai.colaborai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColaboraiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
